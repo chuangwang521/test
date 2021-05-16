8b965fa4d96138c6358d3c3ea5fc2b43886a5e35
cw <382931852@qq.com>
2021-05-16 14:28:01 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import VuetifyDialog from 'vuetify-dialog'
+import 'vuetify-dialog/dist/vuetify-dialog.css' 
+import axios from 'axios'; /* 引入axios进行地址访问*/
+Vue.prototype.$http = axios;
 
 Vue.config.productionTip = false
+Vue.use(VuetifyDialog, {
+  context: {
+  vuetify
+  }
+})
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
